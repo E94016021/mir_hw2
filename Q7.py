b5a1ee264190f0d267e30db3e0f5fbf2d4d75161
exeex <xray0h@gmail.com>
2018-06-12 22:05:39 +0800

    Q7

--- Q7.py
+++ Q7.py
@@ -21,7 +21,7 @@ song_id = 10
 aud, sr = dataset[0]
 
 bpm = librosa.beat.tempo(aud)
-beats_gt = dataset.get_gg(song_id)
+beats_gt, beats_num = dataset.get_gg(song_id)
 print(bpm, dataset.get_gt_bpm(song_id))
 
 onset_frames = librosa.onset.onset_detect(y=aud, sr=sr)
@@ -36,49 +36,56 @@ times = librosa.frames_to_time(np.arange(len(o_env)), sr=sr)
 onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
 
 plt.figure()
-plt.title('Power spectrogram')
 
-# D = librosa.stft(aud)
-# ax1 = plt.subplot(2, 1, 1)
-# librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
-#                          x_axis='time', y_axis='log')
-# plt.subplot(2, 1, 2, sharex=ax1)
+plt.title('Power spectrogram')
 
 plt.plot(times, o_env, label='Onset strength')
 
-# plt.vlines(times[onset_frames], 0, o_env.max(), color='r', alpha=0.9,
-#            linestyle='--', label='Onsets')
-
 plt.vlines(times[beats], 0, 1, alpha=0.5, color='r',
            linestyle='--', label='Beats')
-
-beats = np.array(beats)
-beat_onset_strength = o_env[beats]+o_env[beats+1]+o_env[beats-1]
-
-
-
-# peaks = scipy.signal.find_peaks_cwt(beat_onset_strength, widths=np.arange(1,2))
-
-l = len(beat_onset_strength)
-
-peaks = [i for i in range(1, l - 1)
-         if beat_onset_strength[i] > beat_onset_strength[i - 1] and
-         beat_onset_strength[i] > beat_onset_strength[i + 1]]
-
-# plt.plot(beat_onset_strength)
-# plt.scatter(peaks, beat_onset_strength[peaks])
-
-down_beats = beats[peaks]
-
-# plt.vlines(times[down_beats], 0, 1, alpha=0.5, color='r',
-#            linestyle='--', label='Beats')
-
-
-
 plt.vlines(beats_gt, 0, 1, alpha=0.5, color='g',
-           linestyle='--', label='Beats')
-
+           linestyle='--', label='Beats_GT')
 
 plt.axis('tight')
 plt.legend(frameon=True, framealpha=0.75)
 plt.show()
+
+beats = times[beats]
+
+
+def check_70ms(t, beats):
+    idx = np.where(np.logical_and((t - 0.070) < beats, (t + 0.070) > beats))
+
+    try:
+        x = idx[0][0]
+        return True
+    except IndexError:
+        return False
+
+
+TP = 0
+FN = 0
+FP = 0
+
+for beat in beats:
+    if check_70ms(beat, beats_gt):
+        TP += 1
+    else:
+        FP += 1
+
+for beat_gt in beats_gt:
+
+    if check_70ms(beat_gt, beats):
+        TP += 1
+    else:
+        FN += 1
+TP /= 2
+
+print(TP, FP, FN)
+
+P = TP / (TP + FP)
+R = TP / (TP + FN)
+
+F = 2 * P * R / (P + R)
+
+print("P:", P, "R", R, "F", F)
